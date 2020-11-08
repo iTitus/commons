840cfaf4b7e3d5e79241317762d7e9c4f7645c1b
Segelzwerg <marcel.haas@hhu.de>
2020-11-09 00:58:15 +0100

    FIX: test to compare force with newton

--- src/test/java/io/github/ititus/si/quantity/type/ForceTest.java
+++ src/test/java/io/github/ititus/si/quantity/type/ForceTest.java
@@ -4,6 +4,7 @@ import io.github.ititus.si.quantity.Quantity;
 import org.junit.jupiter.api.Test;
 
 import static io.github.ititus.si.quantity.type.Acceleration.METRES_PER_SECOND_SQUARED;
+import static io.github.ititus.si.quantity.type.Force.FORCE;
 import static io.github.ititus.si.quantity.type.Force.NEWTON;
 import static io.github.ititus.si.quantity.type.Mass.KILOGRAM;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +17,7 @@ public class ForceTest {
 
         Quantity<Force> expectedForce = NEWTON.get(10);
 
-        Quantity<?> force = twoKilos.multiply(acceleration);
+        Quantity<Force> force = twoKilos.multiply(acceleration).as(FORCE).convertTo(NEWTON);
         assertThat(force).isEqualTo(expectedForce);
     }
 
@@ -25,7 +26,7 @@ public class ForceTest {
         Quantity<Mass> twoKilos = KILOGRAM.get(2);
         Quantity<Acceleration> acceleration = METRES_PER_SECOND_SQUARED.get(5);
 
-        Quantity<?> force = twoKilos.multiply(acceleration);
+        Quantity<Force> force = twoKilos.multiply(acceleration).as(FORCE);
         assertThat(force.getUnit().getSymbol()).isEqualTo("kgms^-2");
     }
 }
