2a3039dac6e8dc3f6f41f646557aaa8dcd7b0010
Segelzwerg <marcel.haas@hhu.de>
2020-11-08 19:15:47 +0100

    ADD: test for mass conversion

---
+++ src/test/java/io/github/ititus/si/quantity/type/MassTest.java
@@ -0,0 +1,21 @@
+package io.github.ititus.si.quantity.type;
+
+import io.github.ititus.si.quantity.Quantity;
+import org.junit.jupiter.api.Test;
+
+import static io.github.ititus.si.prefix.MetricPrefix.NANO;
+import static io.github.ititus.si.quantity.type.Mass.GRAM;
+import static io.github.ititus.si.quantity.type.Mass.KILOGRAM;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Percentage.withPercentage;
+
+public class MassTest {
+    @Test
+    public void kilogramToNanoGrams() {
+        Quantity<Mass> kiloGrams = KILOGRAM.get(1.35);
+        Quantity<Mass> nanoGrams = kiloGrams.convertTo(GRAM.prefix(NANO));
+
+        assertThat(nanoGrams.getValue()).isCloseTo(1.35E12, withPercentage(1));
+        assertThat(nanoGrams.getUnit().getSymbol()).isEqualTo("ng");
+    }
+}
