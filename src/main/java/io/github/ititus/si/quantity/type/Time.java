b5550700679727a6c0bb633f39d65cf14b162206
iTitus <iTitus@users.noreply.github.com>
2020-12-03 12:09:34 +0100

    Add interop to Time quantity type for java.time.Duration

--- src/main/java/io/github/ititus/si/quantity/type/Time.java
+++ src/main/java/io/github/ititus/si/quantity/type/Time.java
@@ -3,6 +3,9 @@ package io.github.ititus.si.quantity.type;
 import io.github.ititus.si.dimension.BaseDimension;
 import io.github.ititus.si.quantity.Quantity;
 
+import java.time.Duration;
+
+import static io.github.ititus.si.unit.Units.NANOSECOND;
 import static io.github.ititus.si.unit.Units.SECOND;
 
 public final class Time extends AbstractQuantityType<Time> {
@@ -15,5 +18,16 @@ public final class Time extends AbstractQuantityType<Time> {
 
     public static Quantity<Time> asFrequency(Quantity<Frequency> qty) {
         return qty.inverse().as(TIME);
+    }
+
+    public static Duration asDuration(Quantity<Time> qty) {
+        long seconds = qty.convertTo(SECOND).getValue().longValue();
+        long nanoAdjustment = qty.subtract(SECOND.get(seconds)).convertTo(NANOSECOND).getValue().longValue();
+
+        return Duration.ofSeconds(seconds, nanoAdjustment);
+    }
+
+    public static Quantity<Time> of(Duration d) {
+        return SECOND.get(d.getSeconds()).add(NANOSECOND.get(d.getNano())).convertToStandard();
     }
 }
