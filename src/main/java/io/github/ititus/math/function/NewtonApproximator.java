853cba5c0b0768ae8dd4e530108b66a5226315d8
iTitus <iTitus@users.noreply.github.com>
2020-12-24 00:50:02 +0100

    static imports

--- src/main/java/io/github/ititus/math/function/NewtonApproximator.java
+++ src/main/java/io/github/ititus/math/function/NewtonApproximator.java
@@ -1,13 +1,15 @@
 package io.github.ititus.math.function;
 
 import io.github.ititus.math.number.BigRational;
-import io.github.ititus.math.number.BigRationalConstants;
 
 import java.math.MathContext;
 
+import static io.github.ititus.math.number.BigRational.ofExp;
+import static io.github.ititus.math.number.BigRationalConstants.ONE;
+
 public class NewtonApproximator {
 
-    private static final BigRational ATAN_APPROX_FACTOR = BigRational.ofExp(28, -2);
+    private static final BigRational ATAN_APPROX_FACTOR = ofExp(28, -2);
     private static final int MAX_ITER = 1_000;
 
     public static BigRational atan(BigRational x) {
@@ -20,14 +22,14 @@ public class NewtonApproximator {
             throw new ArithmeticException();
         }
 
-        BigRational minDiff = BigRational.ofExp(1, -precision);
+        BigRational minDiff = ofExp(1, -precision);
 
         int n = 0;
-        BigRational x_n = x.divide(BigRationalConstants.ONE.add(ATAN_APPROX_FACTOR.multiply(x.squared()))).round(mc);
+        BigRational x_n = x.divide(ONE.add(ATAN_APPROX_FACTOR.multiply(x.squared()))).round(mc);
         BigRational diff;
         do {
             BigRational tan = x_n.tan();
-            diff = tan.subtract(x).divide(BigRationalConstants.ONE.add(tan.squared()));
+            diff = tan.subtract(x).divide(ONE.add(tan.squared()));
             x_n = x_n.subtract(diff)/*.round(mc)*/;
         } while (++n < MAX_ITER && diff.abs().compareTo(minDiff) > 0);
 
