608e88030326a63ddc71d2ecf50fb830f076423e
iTitus <iTitus@users.noreply.github.com>
2020-12-24 01:23:15 +0100

    use eps constant for tests

--- src/test/java/io/github/ititus/math/number/BigRationalAtanTests.java
+++ src/test/java/io/github/ititus/math/number/BigRationalAtanTests.java
@@ -5,14 +5,14 @@ import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static io.github.ititus.assertions.Assertions.assertThat;
-import static io.github.ititus.math.number.BigRational.of;
+import static io.github.ititus.math.number.BigRational.ofExp;
 import static io.github.ititus.math.number.BigRationalConstants.*;
 import static io.github.ititus.math.number.BigRationalMath.atan;
 import static org.assertj.core.data.Offset.strictOffset;
 
 public class BigRationalAtanTests {
 
-    private static final Offset<BigRational> EPS = strictOffset(of("0.001"));
+    private static final Offset<BigRational> EPS = strictOffset(ofExp(1, -3));
 
     @Test
     public void atan_zero() {
