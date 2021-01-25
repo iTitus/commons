7e36a7cff1fb9d4454260b8ad8cf751ba2b831ef
Segelzwerg <marcel.haas@hhu.de>
2021-01-25 12:49:17 +0100

    ADD: test for abs squared

--- src/test/java/io/github/ititus/math/number/BigComplexTests.java
+++ src/test/java/io/github/ititus/math/number/BigComplexTests.java
@@ -3,8 +3,11 @@ package io.github.ititus.math.number;
 import org.junit.jupiter.api.Test;
 
 import static io.github.ititus.assertions.Assertions.assertThat;
+import static io.github.ititus.math.number.BigRational.of;
+import static org.assertj.core.data.Offset.offset;
 
 public class BigComplexTests {
+    public static final BigComplex TWO_I_TWO = BigComplex.of(BigRational.of(2), BigRational.of(2));
     @Test
     public void test_one_real() {
         assertThat(BigComplex.ONE.getReal()).isOne();
@@ -13,5 +16,10 @@ public class BigComplexTests {
     @Test
     public void test_one_imag() {
         assertThat(BigComplex.ONE.getImag()).isZero();
+    }
+
+    @Test
+    public void test_abs_squared() {
+        assertThat(TWO_I_TWO.absSquared()).isCloseTo(BigRational.of(8), offset(of("0.001")));
     }
 }
