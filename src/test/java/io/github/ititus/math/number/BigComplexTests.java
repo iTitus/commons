a90f63f16adc05f07e5fcc6ed76ace8c446a96bd
Segelzwerg <marcel.haas@hhu.de>
2021-01-25 13:08:03 +0100

    REFACTOR: extract offset constant

--- src/test/java/io/github/ititus/math/number/BigComplexTests.java
+++ src/test/java/io/github/ititus/math/number/BigComplexTests.java
@@ -1,14 +1,17 @@
 package io.github.ititus.math.number;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static io.github.ititus.assertions.Assertions.assertThat;
 import static io.github.ititus.math.number.BigRational.of;
+import static io.github.ititus.math.number.BigRationalConstants.PI;
 import static org.assertj.core.data.Offset.offset;
 
 public class BigComplexTests {
     public static final BigComplex TWO_I_TWO = BigComplex.of(BigRational.of(2), BigRational.of(2));
     public static final BigRational TWICE_TWO_SQRT = BigRational.of(2).sqrt().multiply(BigRational.of(2));
+    public static final Offset<BigRational> EPSILON = offset(of("0.001"));
 
     @Test
     public void test_one_real() {
@@ -22,12 +25,11 @@ public class BigComplexTests {
 
     @Test
     public void test_abs_squared() {
-        assertThat(TWO_I_TWO.absSquared()).isCloseTo(BigRational.of(8), offset(of("0.001")));
+        assertThat(TWO_I_TWO.absSquared()).isCloseTo(BigRational.of(8), EPSILON);
     }
 
     @Test
     public void test_abs() {
-        assertThat(TWO_I_TWO.abs()).isCloseTo(TWICE_TWO_SQRT,
-                offset(of("0.001")));
+        assertThat(TWO_I_TWO.abs()).isCloseTo(TWICE_TWO_SQRT, EPSILON);
     }
 }
