52ec336f2b08f185a5ee4044959dca84e83df70f
Segelzwerg <marcel.haas@hhu.de>
2021-01-25 12:42:38 +0100

    ADD: test for real part

---
+++ src/test/java/io/github/ititus/math/number/BigComplexTests.java
@@ -0,0 +1,12 @@
+package io.github.ititus.math.number;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.ititus.assertions.Assertions.assertThat;
+
+public class BigComplexTests {
+    @Test
+    public void test_one_real() {
+        assertThat(BigComplex.ONE.getReal()).isOne();
+    }
+}
