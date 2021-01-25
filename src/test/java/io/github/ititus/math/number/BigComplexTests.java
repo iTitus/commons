7192de6236a885d6beff379362358baacd788efb
Segelzwerg <marcel.haas@hhu.de>
2021-01-25 12:59:48 +0100

    FIX: after main merge

--- src/test/java/io/github/ititus/math/number/BigComplexTests.java
+++ src/test/java/io/github/ititus/math/number/BigComplexTests.java
@@ -7,17 +7,18 @@ import static io.github.ititus.math.number.BigRational.of;
 import static org.assertj.core.data.Offset.offset;
 
 public class BigComplexTests {
+    public static final BigComplex ONE = BigComplex.of(BigRational.of(1), BigRational.of(0));
     public static final BigComplex TWO_I_TWO = BigComplex.of(BigRational.of(2), BigRational.of(2));
     public static final BigRational TWICE_TWO_SQRT = BigRational.of(2).sqrt().multiply(BigRational.of(2));
 
     @Test
     public void test_one_real() {
-        assertThat(BigComplex.ONE.getReal()).isOne();
+        assertThat(ONE.getReal()).isOne();
     }
 
     @Test
     public void test_one_imag() {
-        assertThat(BigComplex.ONE.getImag()).isZero();
+        assertThat(ONE.getImag()).isZero();
     }
 
     @Test
