6d6782d016e897a17303adc2f75b989fbdf616dc
iTitus <iTitus@users.noreply.github.com>
2021-06-17 18:22:01 +0200

    fix matrix multiply

--- src/main/java/io/github/ititus/math/matrix/Mat4f.java
+++ src/main/java/io/github/ititus/math/matrix/Mat4f.java
@@ -279,10 +279,10 @@ public final class Mat4f {
 
     public Mat4f multiply(Mat4f o) {
         return new Mat4f(
-                m11 * o.m11 + m12 * o.m21 + m13 * o.m31 + m13 * o.m41, m11 * o.m12 + m12 * o.m22 + m13 * o.m32 + m13 * o.m42, m11 * o.m13 + m12 * o.m23 + m13 * o.m33 + m13 * o.m43, m11 * o.m14 + m12 * o.m24 + m13 * o.m34 + m13 * o.m44,
-                m21 * o.m11 + m22 * o.m21 + m23 * o.m31 + m13 * o.m41, m21 * o.m12 + m22 * o.m22 + m23 * o.m32 + m13 * o.m42, m21 * o.m13 + m22 * o.m23 + m23 * o.m33 + m13 * o.m43, m21 * o.m14 + m22 * o.m24 + m23 * o.m34 + m13 * o.m44,
-                m31 * o.m11 + m32 * o.m21 + m33 * o.m31 + m13 * o.m41, m31 * o.m12 + m32 * o.m22 + m33 * o.m32 + m13 * o.m42, m31 * o.m13 + m32 * o.m23 + m33 * o.m33 + m13 * o.m43, m31 * o.m14 + m32 * o.m24 + m33 * o.m34 + m13 * o.m44,
-                m41 * o.m11 + m42 * o.m21 + m43 * o.m31 + m13 * o.m41, m41 * o.m12 + m42 * o.m22 + m43 * o.m32 + m13 * o.m42, m41 * o.m13 + m42 * o.m23 + m43 * o.m33 + m13 * o.m43, m41 * o.m14 + m42 * o.m24 + m43 * o.m34 + m13 * o.m44
+                m11 * o.m11 + m12 * o.m21 + m13 * o.m31 + m14 * o.m41, m11 * o.m12 + m12 * o.m22 + m13 * o.m32 + m14 * o.m42, m11 * o.m13 + m12 * o.m23 + m13 * o.m33 + m14 * o.m43, m11 * o.m14 + m12 * o.m24 + m13 * o.m34 + m14 * o.m44,
+                m21 * o.m11 + m22 * o.m21 + m23 * o.m31 + m24 * o.m41, m21 * o.m12 + m22 * o.m22 + m23 * o.m32 + m24 * o.m42, m21 * o.m13 + m22 * o.m23 + m23 * o.m33 + m24 * o.m43, m21 * o.m14 + m22 * o.m24 + m23 * o.m34 + m24 * o.m44,
+                m31 * o.m11 + m32 * o.m21 + m33 * o.m31 + m34 * o.m41, m31 * o.m12 + m32 * o.m22 + m33 * o.m32 + m34 * o.m42, m31 * o.m13 + m32 * o.m23 + m33 * o.m33 + m34 * o.m43, m31 * o.m14 + m32 * o.m24 + m33 * o.m34 + m34 * o.m44,
+                m41 * o.m11 + m42 * o.m21 + m43 * o.m31 + m44 * o.m41, m41 * o.m12 + m42 * o.m22 + m43 * o.m32 + m44 * o.m42, m41 * o.m13 + m42 * o.m23 + m43 * o.m33 + m44 * o.m43, m41 * o.m14 + m42 * o.m24 + m43 * o.m34 + m44 * o.m44
         );
     }
 
---
+++ src/test/java/io/github/ititus/matrix/Mat4fTest.java
@@ -0,0 +1,33 @@
+package io.github.ititus.matrix;
+
+import io.github.ititus.math.matrix.Mat4f;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Mat4fTest {
+
+    @Test
+    void testMultiply() {
+        Mat4f a = new Mat4f(
+                1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+                13, 14, 15, 16
+        );
+        Mat4f b = new Mat4f(
+                17, 18, 19, 20,
+                21, 22, 23, 24,
+                25, 26, 27, 28,
+                29, 30, 31, 32
+        );
+        Mat4f c = new Mat4f(
+                250, 260, 270, 280,
+                618, 644, 670, 696,
+                986, 1028, 1070, 1112,
+                1354, 1412, 1470, 1528
+        );
+
+        assertThat(a.multiply(b)).isEqualTo(c);
+    }
+}
