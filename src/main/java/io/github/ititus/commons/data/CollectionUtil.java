bb4e92f45d591dbfb872a569272e19db75a922eb
iTitus <iTitus@users.noreply.github.com>
2022-02-09 12:09:18 +0100

    add "to enum set" methods

--- src/main/java/io/github/ititus/commons/data/CollectionUtil.java
+++ src/main/java/io/github/ititus/commons/data/CollectionUtil.java
@@ -1,6 +1,6 @@
 package io.github.ititus.commons.data;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -11,5 +11,27 @@ public final class CollectionUtil {
 
     public static <T, R> List<R> deepMap(List<? extends T> list, Function<? super T, ? extends R> fct) {
         return list.stream().map(fct).collect(Collectors.toList());
+    }
+
+    @SafeVarargs
+    private static <T extends Enum<T>> EnumSet<T> toEnumSet(T... ts) {
+        return toEnumSet(Arrays.asList(ts));
+    }
+
+    private static <T extends Enum<T>> EnumSet<T> toEnumSet(Collection<T> ts) {
+        return EnumSet.copyOf(ts);
+    }
+
+    @SafeVarargs
+    private static <T extends Enum<T>> Set<T> toUnmodifiableEnumSet(T... ts) {
+        return toUnmodifiableEnumSet(Arrays.asList(ts));
+    }
+
+    private static <T extends Enum<T>> Set<T> toUnmodifiableEnumSet(Collection<T> ts) {
+        if (ts.isEmpty()) {
+            return Set.of();
+        }
+
+        return Collections.unmodifiableSet(EnumSet.copyOf(ts));
     }
 }
